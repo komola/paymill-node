d12b2ea803141497fcbf28a0b08f2a9deded21df
Nikolas Poniros <nikolas@poniros.de>
2013-08-10 16:44:04 +0200

    Add qunit test_runner

---
+++ tests/test_runner.js
@@ -0,0 +1,33 @@
+var util = require('util');
+var testrunner = require('qunit');
+
+// change any option for all tests globally
+testrunner.options.log = {
+    // log assertions overview
+    assertions : false,
+    // log expected and actual values for failed tests
+    errors : true,
+    // log tests overview
+    tests : true,
+    // log summary
+    summary : true,
+    // log global summary (all files)
+    globalSummary : false,
+    // log currently testing code file
+    testing : false
+};
+
+var callback = function (err, report) {
+    util.log('Test done!');
+    if (err) {
+        util.log(util.inspect(err));
+    } else {
+        util.log(util.inspect(report));
+    }
+};
+
+// one code and tests file
+testrunner.run({
+    code : '../lib/main.js',
+    tests : 'payments_test.js'
+}, callback);
